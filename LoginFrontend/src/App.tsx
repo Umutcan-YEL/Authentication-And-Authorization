9062bf3a2ca481127a56d3a12b28568983af9fcf
Umutcan-YEL <umutcanyel132@gmail.com>
2024-02-11 00:46:05 +0300

    code cleaning

--- LoginFrontend/src/App.tsx
+++ LoginFrontend/src/App.tsx
@@ -1,15 +1,9 @@
-import { BrowserRouter, Navigate } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import Routes from "./Routes";
-import { Layout, Menu } from "antd";
-import { useSelector } from "react-redux";
-import { StateModel } from "./models/StateModel";
-import { useState } from "react";
-const { Header } = Layout;
 function App() {
-
   return (
     <BrowserRouter>
-        <Routes />
+      <Routes />
     </BrowserRouter>
   );
 }
